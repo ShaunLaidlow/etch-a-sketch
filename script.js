030ba47ed0efaa3036a7a8197ad8806a0dcafb11
Shaun <shaun.laidlow@gmail.com>
2023-05-11 07:21:40 -0700

    Add Button To Create Custom Grid
    Added button that when clicked prompts the user
    for the amount of columns and rows they want. Cleaned
    up js into more secint functions

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+
 let container = document.querySelector(".container");
 let row = 5;
 let col = 5;
-for(let i = 0; i < row; i++){
-    let rowContainer = document.createElement('div');
-    rowContainer.classList.add("row")
-    for(let j = 0; j < col; j++){
-        let div = document.createElement('div');
-        div.classList.add("box")
-        rowContainer.appendChild(div)
-        }
-    container.appendChild(rowContainer)
+createGrid(row,col);
+
+function createGrid(i,j){
+    for(i = 0; i < row; i++){
+        let rowContainer = document.createElement('div');
+        rowContainer.classList.add("row")
+        for(j = 0; j < col; j++){
+            let div = document.createElement('div');
+            div.classList.add("box")
+            rowContainer.appendChild(div)
+            }
+        container.appendChild(rowContainer)
+    }
 }
 
 function randomColor(){
@@ -19,28 +24,53 @@ function randomColor(){
     return `rgb(${r}, ${g}, ${b})`
 }
 
+function listenBoxes(){
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => {
+        let styles = window.getComputedStyle(box);
+        let backgroundColour
+        let brightness = 1;
+        box.addEventListener("mouseover", function(e){ 
+            backgroundColour = styles.getPropertyValue("background-color")
+          
+        })
+        
+        box.addEventListener("mouseout", function(e){
+            if(backgroundColour == "rgb(0, 0, 0)"){
+                e.target.style.background = randomColor();
+            }
+            else{
+               let filter = styles.getPropertyValue("filter")
+               brightness -= 0.2
+               e.target.style.filter = `brightness(${brightness})` 
+               if(brightness<=0){
+                brightness=1.2;
+               }
+            }
+        })
+    });
+}
+listenBoxes()
 
-let boxes = document.querySelectorAll(".box");
-boxes.forEach(box => {
-    let styles = window.getComputedStyle(box);
-    let backgroundColour
-    let brightness = 1;
-    box.addEventListener("mouseover", function(e){ 
-        backgroundColour = styles.getPropertyValue("background-color")
-        console.log(backgroundColour)
-    })
-    
-    box.addEventListener("mouseout", function(e){
-        if(backgroundColour == "rgb(0, 0, 0)"){
-            e.target.style.background = randomColor();
-        }
-        else{
-           let filter = styles.getPropertyValue("filter")
-           brightness -= 0.2
-           e.target.style.filter = `brightness(${brightness})` 
-        }
-        
-        
-        
-    })
-});
+
+let button = document.querySelector("#button");
+console.log(button)
+button.addEventListener("click",function(){
+    chooseGrid();
+})
+
+
+function chooseGrid(){
+    row = prompt("How many rows do you want?")
+    col = prompt("How many columns?")
+    deleteGrid()
+    createGrid(row,col)
+    listenBoxes()
+}
+
+function deleteGrid(){
+    let rowContainer = document.querySelectorAll(".row");
+    rowContainer.forEach(row => {
+        container.removeChild(row)
+    });
+}
